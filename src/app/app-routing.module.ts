9313280f7cc0b356396543167456f3556e642298
MaK <manishkl543@gmail.com>
2023-10-11 07:18:16 +0530

    Initial layout commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: ContentComponent},
+  { path: 'users', component: ContentComponent},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    SidebarComponent,
+    ContentComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
